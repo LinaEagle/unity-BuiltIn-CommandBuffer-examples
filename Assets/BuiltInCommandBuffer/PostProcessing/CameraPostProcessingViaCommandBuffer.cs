730d804e11c75d13487a2b0b1a94eece424ca723
apollinariia <apollinariia.orlova@gmail.com>
2022-10-18 12:55:24 +0300

    code cleanup

--- Assets/BuiltInCommandBuffer/PostProcessing/CameraPostProcessingViaCommandBuffer.cs
+++ Assets/BuiltInCommandBuffer/PostProcessing/CameraPostProcessingViaCommandBuffer.cs
@@ -10,12 +10,11 @@ public class CameraPostProcessingViaCommandBuffer : MonoBehaviour
     private Camera _camera;
     private CommandBuffer _cBuffer;
 
-    private int _screenCopyID = Shader.PropertyToID("_MainTex");
+    private readonly int _screenCopyID = Shader.PropertyToID("_MainTex");
 
     private void Awake()
     {
         _camera = GetComponent<Camera>();
-        _camera.forceIntoRenderTexture = true;
     }
 
     private void OnPreRender()
@@ -28,35 +27,11 @@ public class CameraPostProcessingViaCommandBuffer : MonoBehaviour
         CommandBufferUtils.Destroy(_cBuffer, _commandBufferName, _cameraEvent, _camera);
         _cBuffer = CommandBufferUtils.CreateNew(_commandBufferName);
 
-        // Attempt1();
-        // Attempt2();
-        Attempt3();
-
-        _camera.AddCommandBuffer(_cameraEvent, _cBuffer);
-    }
-
-    // flips image vertically
-    private void Attempt1()
-    {
-        // -1 means current screen size
         _cBuffer.GetTemporaryRT(_screenCopyID, -1, -1);
-        _cBuffer.Blit(BuiltinRenderTextureType.CurrentActive, _screenCopyID);
-        _cBuffer.Blit(_screenCopyID, BuiltinRenderTextureType.CurrentActive, _material);
-        _cBuffer.ReleaseTemporaryRT(_screenCopyID);
-    }
-    
-    // renders white screen
-    private void Attempt2()
-    {
-        _cBuffer.Blit(BuiltinRenderTextureType.CurrentActive, BuiltinRenderTextureType.CurrentActive, _material);
-    }
-    
-    private void Attempt3()
-    {
-        _cBuffer.GetTemporaryRT(_screenCopyID, -1, -1);
-        //_cBuffer.SetRenderTarget(_screenCopyID);
         _cBuffer.Blit(BuiltinRenderTextureType.CameraTarget, _screenCopyID, _material);
         _cBuffer.Blit(_screenCopyID, BuiltinRenderTextureType.CameraTarget);
         _cBuffer.ReleaseTemporaryRT(_screenCopyID);
+
+        _camera.AddCommandBuffer(_cameraEvent, _cBuffer);
     }
 }
